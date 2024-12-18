d551a9cdec9bdadfa0800998a4357a693f478e60
SimoneMarconi <simo.marconi03@gmail.com>
2024-12-18 22:06:00 +0100

    added first index.html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return send_from_directory("./templates", "index.html")
 
 
 if __name__ == "__main__":
